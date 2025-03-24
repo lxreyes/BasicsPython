f0389cba59447227d0251c7c4e7dad8ba8fc5a50
Lachlan <lachlan.reyes@icloud.com>
2025-03-23 20:59:24 -0400

    Put in coinflip simulation

--- unit3/main.py
+++ unit3/main.py
@@ -1,5 +1,22 @@
+import random
 
 
+
+N = 100
+numheads = 0
+
+#roll = random.randint(1,6)
+#print(roll)
+
+for _ in range(N):
+    flip_int = random.randint(1,100)
+    if flip_int <= 30:
+        numheads = numheads + 1
+
+print (numheads / N * N)
+
+
+ 
 
 
 
